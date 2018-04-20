fa58dd9c5daa81ecc3a628f31a54577684190aa2
heramareh <8926924@qq.com>
2018-04-20 20:14:46 +0800

    update

--- sign/views.py
+++ sign/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
-
+from django.contrib import auth
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def index(request):
@@ -12,15 +13,18 @@ def login_action(request):
     if request.method == 'POST':
         username = request.POST.get('username', '')
         password = request.POST.get('password', '')
-        if username == 'admin' and password == 'admin':
+        user = auth.authenticate(username=username, password=password)
+        if user is not None:
+            auth.login(request, user)   # 登录
+            request.session['user'] = username  # 将session信息记录到浏览器
             # return HttpResponseRedirect('/event_manage/')
             response = HttpResponseRedirect('/event_manage/')
             # response.set_cookie('user', username, 3600)   # 添加浏览器cookie
-            request.session['user'] = username  # 将session信息记录到浏览器
             return response
         else:
             return render(request, 'index.html', {'error': 'username or password error!'})
 
+@login_required
 def event_manage(request):
     u"""发布会管理"""
     # return render(request, "event_manage.html")
